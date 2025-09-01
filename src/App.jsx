7c187f5fd6878b85342de17727d997556dc4640e
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-01 15:07:18 +0200

    feat(installazione boostrap  e creazione array di oggetti in app.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from 'react'
+// Dati iniziali
+const initialMovies = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
 
 function App() {
   
